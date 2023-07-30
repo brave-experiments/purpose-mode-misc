50242aba07b6cc0e1cbf4f214ca8c7693062e9b0
Hank Lee <hankhplee@gmail.com>
2023-07-30 15:02:33 -0400

    Update user overview script - the script take an argument mode and support showing status for active participants and testing ones

--- scripts/user_overview.py
+++ scripts/user_overview.py
@@ -2,7 +2,8 @@ import json
 import pandas as pd
 from prettytable import PrettyTable
 import time
-from pid import active_pid
+from pid import active_pid, testing_pid
+import sys
 
 def get_unixtime(dt64):
     return dt64.astype('datetime64[s]').astype('int')
@@ -17,10 +18,9 @@ def getActivatedFeatures(site,data):
             "Autoplay"
         ]
     active = []
-    if site == "Twitter":
-        for feature in feature_list:
-            if(data[site+feature].values[0]):
-                active.push(feature)
+    for feature in feature_list:
+        if(data[site+feature].values[0]):
+            active.append(feature)
     return active
 
 table = PrettyTable()
@@ -35,33 +35,45 @@ table.field_names = ["UID",
                      "LinkedInFeature",
                      "FacebookFeature",
                      ]
+def main(mode):
+    if (mode == "active"):
+        pids = active_pid
+    elif (mode == "test"):
+        pids = testing_pid
+    else:
+        print("Unknown pid parameter: ",mode)
+        return
 
-for pid in active_pid:
-    current_time = int(time.time())
-    # read file
-    df = pd.read_json('../../purpose-mode-data/'+pid+'/ping.json', lines=True)
+    for pid in pids:
+        current_time = int(time.time())
+        # read file
+        df = pd.read_json('../../purpose-mode-data/'+pid+'/ping.json', lines=True)
 
-    try: 
-        feature_questionnaire = pd.read_json('./'+pid+'/feature_questionnaire.json', lines=True)
-        feature_q_counter = feature_questionnaire.index
-    except:
-        feature_q_counter = 0
+        try: 
+            feature_questionnaire = pd.read_json('../../purpose-mode-data/'+pid+'/feature_feedback_questionnaire.json', lines=True)
+            feature_q_counter = feature_questionnaire.shape[0]
+        except:
+            feature_q_counter = 0
 
-    last_ping = df.iloc[-1:]
+        last_ping = df.iloc[-1:]
 
-    user = [
-        last_ping['uid'].values[0],
-        int((current_time - get_unixtime(last_ping["timestamp"].values[0]))/60),
-        last_ping['Enable'].values[0],
-        last_ping['enableIntervention'].values[0],
-        last_ping['esm_counter_total'].values[0],
-        feature_q_counter,
-        getActivatedFeatures("Twitter",last_ping),
-        getActivatedFeatures("YouTube",last_ping),
-        getActivatedFeatures("LinkedIn",last_ping),
-        getActivatedFeatures("Facebook",last_ping),
-    ]
+        user = [
+            last_ping['uid'].values[0],
+            int((current_time - get_unixtime(last_ping["timestamp"].values[0]))/60),
+            last_ping['Enable'].values[0],
+            last_ping['enableIntervention'].values[0],
+            last_ping['esm_counter_total'].values[0],
+            feature_q_counter,
+            getActivatedFeatures("Twitter",last_ping),
+            getActivatedFeatures("YouTube",last_ping),
+            getActivatedFeatures("LinkedIn",last_ping),
+            getActivatedFeatures("Facebook",last_ping),
+        ]
+        table.add_rows([user])
+    
+    print(table)
 
-    table.add_rows([user])
-
-print(table)
+# use: python user_over.py {active/test}
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    main(mode)
