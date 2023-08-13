76f0d84db762889fb1dae2702d943f6499310b59
Hank Lee <hankhplee@gmail.com>
2023-08-13 02:22:08 -0400

    update pid

--- scripts/pid.py
+++ scripts/pid.py
@@ -1,16 +1,19 @@
 active_pid = {
-    "Pilot_1v6",
-    "Pilot_3v3",
-    "sreehari",
-    "Masood"
+    "ys",
+    "Pilot_1v7"
 }
 
 done_pid = {
-    "Harald_v2"
+    # pilot
+    "Harald_v2",
+    "sreehari",
+    "Masood",
+    "Pilot_3v3"
+
 }
 
 testing_pid = {
-    "Hankv5",
+    "Hankv7",
     "Hank-Chrome",
     "Hank_Brave",
     "Hank_Brave-Chrome"
